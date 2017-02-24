9df87266e5ad55bd3746930d8dddbef1038b8b89
caonuan <caonuan888@163.com>
2017-02-24 19:16:09 +0800

    try-catch可能出错的添加ip信息代码（主键重复错误）

--- src/main/java/pblog/util/VisitorLoginInterceptor.java
+++ src/main/java/pblog/util/VisitorLoginInterceptor.java
@@ -41,17 +41,23 @@ public class VisitorLoginInterceptor extends AbstractInterceptor {
 			session.put("ip", ip);
 			loginByIP(ip, session);
 		}
-		//判断表中是否有ip信息，没有则查询并保存
-		IPInfo ipInfo=visitorService.getIPInfoByIP(ip);
-		if(ipInfo==null){
-			Map<Integer, JSONObject> map=AddressUtils.getAddressByIp(ip); 
-			ipInfo=Util.getIPInfoByJson(map);
-			visitorService.saveIPInfoByJson(ipInfo);
+		// 判断表中是否有ip信息，没有则查询并保存
+		// 17-2-24发现即使判断出表中有ip信息也可能进入存储ip信息的代码段，所以加了个try-catch
+		IPInfo ipInfo = visitorService.getIPInfoByIP(ip);
+		if (ipInfo == null) {
+			Map<Integer, JSONObject> map = AddressUtils.getAddressByIp(ip);
+			ipInfo = Util.getIPInfoByJson(map);
+			try {
+				visitorService.saveIPInfoByJson(ipInfo);
+			} catch (Exception e) {
+				;
+			}
 		}
-		
-		//保存访问信息
-		Visit visit=new Visit( new Date(), Util.getRequstUrl(request), AddressUtils.getPositionByIPInfo(ipInfo), ipInfo);
-		if(!ip.equals("127.0.0.1")){
+
+		// 保存访问信息
+		Visit visit = new Visit(new Date(), Util.getRequstUrl(request),
+				AddressUtils.getPositionByIPInfo(ipInfo), ipInfo);
+		if (!ip.equals("127.0.0.1")) {
 			visitorService.saveVisitInfo(visit);
 		}
 		if (visitor != null) {
